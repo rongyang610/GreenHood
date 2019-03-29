0cd3bd6cbf9f9d2ff9ab4c49077f71319dbb5aea
JJ Yang <rongyang610@gmail.com>
2019-03-29 14:25:15 -0400

    Fixed a logo positioning

--- frontend/components/Home/greenhood_index.jsx
+++ frontend/components/Home/greenhood_index.jsx
@@ -9,8 +9,8 @@ class GreenhoodIndex extends React.Component {
     return (
       <div>
         <div classname="right-nav">
-          <a href="/"><img src={window.navLogo} className='navLogo' /></a>
-          greenhood
+        <a href="/" div className="logo">
+        <img src={window.navLogo} className='navLogo' /> greenhood </a>
         </div>
         <h1>Welcome to Greenhood, {currentUser.username}! </h1>
         <button onClick={() => logout()}>Log out</button>
@@ -19,9 +19,9 @@ class GreenhoodIndex extends React.Component {
     } else {
       return (
         <div>
-          <div classname="right-nav">
-            <a href="/"><img src={window.navLogo} className='navLogo signup-logo' /></a>
-            greenhood
+          <div className="right-nav">
+            <a href="/" div className="logo">
+            <img src={window.navLogo} className='navLogo' /> greenhood </a>
           </div>
           <Link to={'/signup'}>Sign Up</Link>
           <br/>
