0d126cecb8f8e70f4aadff168e31b4293b89dabf
JJ Yang <rongyang610@gmail.com>
2019-04-04 14:40:04 -0400

    change App.jsx again

--- frontend/components/Home/greenhood_index_container.js
+++ frontend/components/Home/greenhood_index_container.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux';
 import GreenhoodIndex from './greenhood_index';
 import { logout } from '../../actions/session_actions';
 
+console.log('hello');
+
 const msp = ({entities, session }) => {
     return {
         currentUser: entities.users[session.id]
