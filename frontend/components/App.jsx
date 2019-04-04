9cf0dcb7f40f9da1f3840ce408629982c482896a
JJ Yang <rongyang610@gmail.com>
2019-04-04 14:30:13 -0400

    make exact file extensions in App.jsx

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
-import GreenhoodIndexContainer from './home/greenhood_index_container';
-import LoginFormContainer from './auth/login_form_container';
-import SignupFormContainer from './auth/signup_form_container';
-import CryptoContainer from './crypto/crypto_container';
+import GreenhoodIndexContainer from './home/greenhood_index_container.js';
+import LoginFormContainer from './auth/login_form_container.js';
+import SignupFormContainer from './auth/signup_form_container.js';
+import CryptoContainer from './crypto/crypto_container.js';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
