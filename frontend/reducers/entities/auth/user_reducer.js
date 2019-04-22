4c0e9eca5f0e8aa9fc05e9187f002b0afc15b2dd
JJ Yang <rongyang610@gmail.com>
2019-04-22 09:53:10 -0400

    Cleared tradehistory and users from entity on logout

--- frontend/reducers/entities/auth/user_reducer.js
+++ frontend/reducers/entities/auth/user_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_CURRENT_USER } from '../../../actions/session_actions';
+import { RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER } from '../../../actions/session_actions';
 import { merge } from 'lodash';
 
 export default (state={}, action) => {
@@ -6,6 +6,8 @@ export default (state={}, action) => {
     switch (action.type) {
         case RECEIVE_CURRENT_USER:        
             return merge({}, state, { [action.currentUser.id]: action.currentUser });
+        case LOGOUT_CURRENT_USER:
+            return {}; 
         default:
             return state;
     }
