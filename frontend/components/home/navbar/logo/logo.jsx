3b3e2935e1d266b12b5f00d72cbb7b71e6c24fc5
JJ Yang <rongyang610@gmail.com>
2019-05-09 11:33:43 -0400

    css change in logo no nav user

--- frontend/components/home/navbar/logo/logo.jsx
+++ frontend/components/home/navbar/logo/logo.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Logo = () => {
   return (
     <Link to='/' className="logo">
-      <img src={window.navLogo} className='navLogo' /> greenhood 
+      <img src={window.navLogo} className='navLogo' /> <span className="logo-name">Greenhood </span> 
     </Link>
     );
 };
